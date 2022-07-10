f93441b4d33f1d47218a6312dc3c84bcef0441b4
Carlos Villalobos <carliyipxd@gmail.com>
2022-07-10 14:00:05 -0500

    Add windows platform warning in core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,7 @@
-const fsuipc = require('bindings')('fsuipc.node');
-
-module.exports = fsuipc;
+if (process.platform !== 'win32') {
+  throw new Error('FSUIPC only works on Windows platform');
+} else {
+  const core = require('bindings')('fsuipc.node');
+  
+  module.exports = core;
+}
